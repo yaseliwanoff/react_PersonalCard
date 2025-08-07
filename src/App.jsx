96829fe7167bc3c9acceb6143279f4201acfae64
= <yselivanov857@gmail.com>
2025-08-07 14:00:16 +0300

    replace BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route to="/" element={<Layout />}>
           <Route index element={<Home />}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
